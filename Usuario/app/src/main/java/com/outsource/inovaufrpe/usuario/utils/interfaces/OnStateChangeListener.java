ec105fd686a4edfa104154049d1ecf9fc0a723a1
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2017-11-25 17:20:39 -0300

    Implementação do botão swipe
    Ainda não finalizado

---
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/utils/interfaces/OnStateChangeListener.java
@@ -0,0 +1,9 @@
+package com.outsource.inovaufrpe.usuario.utils.interfaces;
+
+/**
+ * Created by gabri on 25/11/2017.
+ */
+
+public interface OnStateChangeListener {
+    void onStateChange(boolean active);
+}
