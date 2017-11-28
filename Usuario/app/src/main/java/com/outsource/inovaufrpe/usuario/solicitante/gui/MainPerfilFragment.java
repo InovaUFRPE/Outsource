1d2f93914d349dd1c13cd80183228c102504b3f6
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2017-11-27 22:26:23 -0300

    feat: Tela de Perfil

--- Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/solicitante/gui/MainPerfilFragment.java
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/solicitante/gui/MainPerfilFragment.java
@@ -8,16 +8,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.outsource.inovaufrpe.usuario.R;
-import com.outsource.inovaufrpe.usuario.solicitante.gui.ConfiguracoesActivity;
+import com.outsource.inovaufrpe.usuario.solicitante.dominio.Usuario;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class MainPerfilFragment extends Fragment {
-    Button btnConfig;
+    ImageButton btnConfig;
+    TextView nomeUsuario;
+    TextView emailUsuario;
+    TextView telefoneUsuario;
 
 
     public MainPerfilFragment() {
@@ -28,13 +36,41 @@ public class MainPerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        nomeUsuario = view.findViewById(R.id.tvNomePerfil);
+        emailUsuario = view.findViewById(R.id.tvEmailPerfil);
+        telefoneUsuario = view.findViewById(R.id.tvTelefonePerfil);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference usuarioReference = databaseReference.child("usuario").child(usuarioAtual.getUid());
+                usuarioReference.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Usuario user = dataSnapshot.getValue(Usuario.class);
+                        nomeUsuario.setText(user.getNome()+ " " +user.getSobrenome());
+                        emailUsuario.setText(user.getEmail());
+                        telefoneUsuario.setText(user.getTelefone());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                }
+        );
+
+        nomeUsuario.setText(usuarioAtual.getDisplayName());
+
+        return v;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        btnConfig = (Button) getActivity().findViewById(R.id.bt2);
+        btnConfig = getActivity().findViewById(R.id.imageButton);
         btnConfig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
