36ff7fa45a31843981bfdff6fec28a4b293d2cae
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2017-12-09 12:11:08 -0300

    #487

--- Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/solicitante/gui/EditarPerfilActivity.java
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/solicitante/gui/EditarPerfilActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,33 +14,28 @@ import android.widget.Toast;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.outsource.inovaufrpe.usuario.R;
 import com.outsource.inovaufrpe.usuario.solicitante.dominio.Usuario;
 import com.outsource.inovaufrpe.usuario.utils.FirebaseAux;
 
-public class ConfiguracoesActivity extends AppCompatActivity {
+public class EditarPerfilActivity extends AppCompatActivity {
     private EditText etAtualizaNome;
     private EditText etAtualizaSobrenome;
     private EditText etAtualizaEmail;
     private EditText etAtualizaTelefone;
-    private Button btAtualizarID;
-    private Button btDeletarID;
-    private Button btSair;
     private FirebaseAux firebase;
     private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configuracoes);
-        setTitle("Configurações");
+        setContentView(R.layout.activity_editar_perfil);
+        setTitle("Editar Perfil");
         firebase = FirebaseAux.getInstancia();
 
         DatabaseReference firebasereference = firebase.getUsuarioReference().child(firebase.getUser().getUid());
@@ -57,21 +54,13 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
             }
         });
-        etAtualizaNome = (EditText) findViewById(R.id.etAtualizaNomeID);
-        etAtualizaSobrenome = (EditText) findViewById(R.id.etAtualizaSobrenomeID);
-        etAtualizaEmail = (EditText) findViewById(R.id.etAtualizaEmailID);
-        etAtualizaTelefone = (EditText) findViewById(R.id.etAtualizaTelefoneID);
-        btAtualizarID = (Button) findViewById(R.id.btAtualizarID);
-        btDeletarID = (Button) findViewById(R.id.btDeletarID);
-        btSair = (Button) findViewById(R.id.btSair);
+        etAtualizaNome = findViewById(R.id.etAtualizaNomeID);
+        etAtualizaSobrenome = findViewById(R.id.etAtualizaSobrenomeID);
+        etAtualizaEmail = findViewById(R.id.etAtualizaEmailID);
+        etAtualizaTelefone = findViewById(R.id.etAtualizaTelefoneID);
+        Button btDeletarID = findViewById(R.id.btDeletarID);
+        Button btSair = findViewById(R.id.btSair);
 
-
-        btAtualizarID.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Atualizar();
-            }
-        });
 
         btDeletarID.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,12 +77,12 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         });
     }
 
-    public void Atualizar(){
+    public void Atualizar() {
         user = firebase.getUser();
         user.updateEmail(etAtualizaEmail.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
                     final Usuario usuario = new Usuario();
                     usuario.setId(user.getUid());
                     usuario.setNome(etAtualizaNome.getText().toString().trim());
@@ -113,30 +102,46 @@ public class ConfiguracoesActivity extends AppCompatActivity {
                         }
                     });
                     finish();
-                    startActivity(new Intent(ConfiguracoesActivity.this, MainActivity.class));
-                }
-            }});
-    }
-
-    public void Deletar(){
-        firebase.getUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(ConfiguracoesActivity.this,"Conta desativada com sucesso.",Toast.LENGTH_SHORT).show();
-                    finish();
-                    startActivity(new Intent(ConfiguracoesActivity.this, LoginActivity.class));
+                    startActivity(new Intent(EditarPerfilActivity.this, MainActivity.class));
                 }
             }
         });
     }
 
-    public void Logout(){
-        Toast.makeText(ConfiguracoesActivity.this, "Saiu com sucesso.", Toast.LENGTH_SHORT).show();
+    public void Deletar() {
+        firebase.getUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(EditarPerfilActivity.this, "Conta desativada com sucesso.", Toast.LENGTH_SHORT).show();
+                    finish();
+                    startActivity(new Intent(EditarPerfilActivity.this, LoginActivity.class));
+                }
+            }
+        });
+    }
+
+    public void Logout() {
+        Toast.makeText(EditarPerfilActivity.this, "Saiu com sucesso.", Toast.LENGTH_SHORT).show();
         firebase.getFirebaseAuth().signOut();
         LoginManager.getInstance().logOut();
         finish();
-        startActivity(new Intent(ConfiguracoesActivity.this, LoginActivity.class));
+        startActivity(new Intent(EditarPerfilActivity.this, LoginActivity.class));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.salvar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.salvarBtn) {
+            Atualizar();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/solicitante/gui/MainPerfilFragment.java
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/solicitante/gui/MainPerfilFragment.java
@@ -7,16 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.outsource.inovaufrpe.usuario.R;
 import com.outsource.inovaufrpe.usuario.solicitante.dominio.Usuario;
@@ -74,7 +70,7 @@ public class MainPerfilFragment extends Fragment {
         btnConfig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), ConfiguracoesActivity.class));
+                startActivity(new Intent(getActivity(), EditarPerfilActivity.class));
             }
         });
     }
