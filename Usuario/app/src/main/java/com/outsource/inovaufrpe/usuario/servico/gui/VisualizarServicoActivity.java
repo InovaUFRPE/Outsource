9018f0cfcb712f01754d1c54bb9727f0a90a670e
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-01-10 01:41:04 -0300

    #647

--- Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/servico/gui/VisualizarServicoActivity.java
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/servico/gui/VisualizarServicoActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -482,5 +484,22 @@ public class VisualizarServicoActivity extends AppCompatActivity {
         novaData = novaData.replace(".", "");
         DatabaseReference databaseReferenceComentario = FirebaseDatabase.getInstance().getReference().child("comentario");
         databaseReferenceComentario.child(comentario.getServico()).child(novaData).setValue(comentario);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.opcoes_servico_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.excluirServicoBtn) {
+//            excluirServico();
+        } else {
+//            editarServico();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
