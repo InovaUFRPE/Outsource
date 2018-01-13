0ef77d71d9092fed7c4d15e5fbb80459fac6f40c
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-01-12 23:27:48 -0300

    #645 append

--- Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/utils/swipebutton/interfaces/OnStateChangeListener.java
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/utils/swipebutton/interfaces/OnStateChangeListener.java
@@ -1,4 +1,4 @@
-package com.outsource.inovaufrpe.usuario.utils.interfaces;
+package com.outsource.inovaufrpe.usuario.utils.swipebutton.interfaces;
 
 public interface OnStateChangeListener {
     void onStateChange(boolean active);
