868b9beb149a07bb374def59b1345e9edb73fe60
Unknown <heitor.augusto@ufrpe.br>
2018-01-15 00:35:20 -0300

    #677 - Prestador

---
+++ Prestador/app/src/main/java/com/outsource/inovaufrpe/prestador/servico/gui/NegociacoesActivity.java
@@ -0,0 +1,15 @@
+package com.outsource.inovaufrpe.prestador.servico.gui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.outsource.inovaufrpe.prestador.R;
+
+public class NegociacoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_negociacoes);
+    }
+}
