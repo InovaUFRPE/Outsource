af0b8a3ca0f5925d1979ae8eb36de84460e7ddea
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-01-16 00:09:04 -0300

    #687
    Tela de histórico de serviços acessível pela carteira - Usuário

--- Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/carteira/gui/MainCarteiraFragment.java
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/carteira/gui/MainCarteiraFragment.java
@@ -1,71 +1,40 @@
 package com.outsource.inovaufrpe.usuario.carteira.gui;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.outsource.inovaufrpe.usuario.R;
-import com.outsource.inovaufrpe.usuario.solicitante.dominio.Usuario;
+import com.outsource.inovaufrpe.usuario.solicitante.adapter.TabAdapter;
+import com.outsource.inovaufrpe.usuario.solicitante.gui.MainPerfilFragment;
 
-import java.text.DecimalFormat;
-
-public class MainCarteiraFragment extends Fragment {
-    TextView tvMoedaCarteiraID;
-    Button btAdicionarMoedaID;
-
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-//    FirebaseDatabase firebaseDatabase;
-
+public class MainCarteiraFragment extends android.support.v4.app.Fragment {
 
     public MainCarteiraFragment() {
     }
 
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_carteira, container, false);
-        preencheDados();
-        tvMoedaCarteiraID = view.findViewById(R.id.tvMoedaCarteiraID);
-        btAdicionarMoedaID = view.findViewById(R.id.btAdicionarMoedaID);
-        btAdicionarMoedaID.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), AdicionarMoedaActivity.class));
-            }
-        });
+
+        ViewPager viewPager = view.findViewById(R.id.pager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
     }
 
-    public void preencheDados() {
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        DatabaseReference usuarioreference = databaseReference.child("usuario");
-        final DatabaseReference firebasereference = usuarioreference.child(user.getUid());
-        firebasereference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Usuario usuario = dataSnapshot.getValue(Usuario.class);
-                DecimalFormat df = new DecimalFormat("####0.00");
-                tvMoedaCarteiraID.setText(df.format(Float.parseFloat(String.valueOf(usuario.getCarteira()))).replace(".", ","));
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+    private void setupViewPager(ViewPager viewPager) {
+        TabAdapter adapter = new TabAdapter(getChildFragmentManager());
+        adapter.addFragment(new SaldoFragment(), "Saldo");
+        adapter.addFragment(new HistoricoServicosFragment(), "Histórico");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/carteira/gui/SaldoFragment.java
@@ -0,0 +1,70 @@
+package com.outsource.inovaufrpe.usuario.carteira.gui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.outsource.inovaufrpe.usuario.R;
+import com.outsource.inovaufrpe.usuario.solicitante.dominio.Usuario;
+
+import java.text.DecimalFormat;
+
+public class SaldoFragment extends android.support.v4.app.Fragment {
+    TextView tvMoedaCarteiraID;
+    Button btAdicionarMoedaID;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+//    FirebaseDatabase firebaseDatabase;
+
+
+    public SaldoFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_saldo_carteira, container, false);
+        preencheDados();
+        tvMoedaCarteiraID = view.findViewById(R.id.tvMoedaCarteiraID);
+        btAdicionarMoedaID = view.findViewById(R.id.btAdicionarMoedaID);
+        btAdicionarMoedaID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), AdicionarMoedaActivity.class));
+            }
+        });
+        return view;
+    }
+
+    public void preencheDados() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        DatabaseReference usuarioreference = databaseReference.child("usuario");
+        final DatabaseReference firebasereference = usuarioreference.child(user.getUid());
+        firebasereference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+                DecimalFormat df = new DecimalFormat("####0.00");
+                tvMoedaCarteiraID.setText(df.format(Float.parseFloat(String.valueOf(usuario.getCarteira()))).replace(".", ","));
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
