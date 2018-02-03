d129727cb7f725bb2163391517b474910eac155e
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-02-03 15:51:03 -0300

    Criação da classe Utils

---
+++ Prestador/app/src/main/java/com/outsource/inovaufrpe/prestador/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.outsource.inovaufrpe.prestador.utils;
+
+/**
+ * Created by gabri on 03/02/2018.
+ */
+
+public class Utils {
+}
---
+++ Usuario/app/src/main/java/com/outsource/inovaufrpe/usuario/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.outsource.inovaufrpe.usuario.utils;
+
+/**
+ * Created by gabri on 03/02/2018.
+ */
+
+public class Utils {
+}
